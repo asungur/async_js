676fdd479324a5aabd13799326190c1eb9391a8a
asungur <sunguralican@gmail.com>
2021-11-03 23:05:08 +0000

    update generator code 3

--- generators/gen_ex03.js
+++ generators/gen_ex03.js
@@ -24,12 +24,12 @@ async function* generator(repo) {
     }
 
     // extract the URL of the next page from the headers
-    const link = response.headers.get('Link');  
+    const link = response.headers.get('Link');
     repo = /<(.*?)>; rel="next"/.exec(link)?. [1];
     
     // if there's no "next page", break the loop.
     if (repo === undefined) {
-      break;  
+      break;
     }
   }
 }
@@ -45,7 +45,7 @@ async function getCommits(repo) {
     console.log(commit);
 
     // break at 90 commits
-    if (++i === 90) {  
+    if (++i === 90) {
       break;
     }
   }
